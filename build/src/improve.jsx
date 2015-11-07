b2c0769f994c294a01d1043c89eccc97d83309c2
danieldealday <danieldealday@gmail.com>
2015-11-06 20:56:44 -0800

    working to reactify rank page

--- build/src/improve.jsx
+++ build/src/improve.jsx
@@ -2,64 +2,71 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = require('jquery');
 
-var rankForm = React.createClass({
+var RankForm = React.createClass({
   allowDropStatus: function (event) {
     event.preventDefault();
   	return false;
   },
   dragInitialize: function (event) {
     event.dataTransfer.effectAllowed='move';
-    event.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+    event.dataTransfer.setData("Text", event.target.getAttribute('id'));
     return true;
   },
   dropComplete: function (event) {
     event.preventDefault();
-    var src = ev.dataTransfer.getData("Text");
+    var src = event.dataTransfer.getData("Text");
     event.target.appendChild(document.getElementById(src));
     event.stopPropagation();
     return false;
   },
+  submitForm: function () {
+    var rank1 = document.getElementById('rank1').value;
+    var rank2 = document.getElementById('rank2').value;
+    var rank3 = document.getElementById('rank3').value;
+  },
   render: function () {
-      <div class="form-body">
-        <button id="drag" draggable="true" ondragstart={this.state.dragInitialize} class="button-imp button-imp-block"/>FINANCIAL</button>
-        <button id="drag" draggable="true" ondragstart={this.state.dragInitialize} class="button-imp button-imp-block"/>PERSONAL</button>
-        <button id="drag" draggable="true" ondragstart={this.state.dragInitialize} class="button-imp button-imp-block"/>PROFESSIONAL</button>
+    return (
+      <div>
+        <p class='heading'> Now, which of these areas do you want to improve the most?</p>
 
-        <div class="tab-content">
-          <div id="signup">
-            <h1></h1>
-            <form action="/" method="post">
-              <div class="field-wrap">
-                <div class="dropped" ondrop={this.state.dropComplete} ondragover={this.state.allowDropStatus}>
+        <div class="form-body">
+          <button id="financial" draggable="true" ondragstart={this.state.dragInitialize} class="button-imp button-imp-block">FINANCIAL</button>
+          <button id="personal" draggable="true" ondragstart={this.state.dragInitialize} class="button-imp button-imp-block">PERSONAL</button>
+          <button id="professional" draggable="true" ondragstart={this.state.dragInitialize} class="button-imp button-imp-block">PROFESSIONAL</button>
 
+          <div class="tab-content">
+            <div id="signup">
+              <h1></h1>
+              <form action="/" method="post">
+                <div class="field-wrap">
+                  <div id="rank1" class="dropped" ondrop={this.state.dropComplete} ondragover={this.state.allowDropStatus}>
+
+                  </div>
                 </div>
-              </div>
 
-              <div class="field-wrap">
-                <div class="dropped" ondrop={this.state.dropComplete} ondragover={this.state.allowDropStatus}>
+                <div class="field-wrap">
+                  <div id="rank2" class="dropped" ondrop={this.state.dropComplete} ondragover={this.state.allowDropStatus}>
 
+                  </div>
                 </div>
-              </div>
 
-              <div class="field-wrap">
-                <div class="dropped" ondrop={this.state.dropComplete} ondragover={this.state.allowDropStatus}>
+                <div class="field-wrap">
+                  <div id="rank3" class="dropped" ondrop={this.state.dropComplete} ondragover={this.state.allowDropStatus}>
+                  </div>
                 </div>
-              </div>
-              <button class="next-button next-button-block"/>next</button>
+                <button class="next-button next-button-block" onClick={this.submitForm}>next</button>
 
-            </form>
-          </div>
-          <div class="clear"></div>
-            <!-- <button class="next-button next-button-block"/>next</button> -->
+              </form>
+            </div>
             <div class="clear"></div>
+              <button class="next-button next-button-block">next</button>
+              <div class="clear"></div>
+            </div>
           </div>
-        </div><!-- tab-content -->
-        <!-- <button type="submit" class="next"/>NEXT</button> -->
-    </div> <!-- /form -->
+          <button type="submit" class="next">NEXT</button>
+        </div>
+    )
   }
 });
 
-// <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-// <script src="js/index.js"></script>
-
-ReactDOM.render(<SignInPage />, document.getElementById('SignUp'));
+ReactDOM.render(<RankForm />, document.getElementById('improvement'));
