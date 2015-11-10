413ed4d7672c7dacc915d38d6bd33c902a938724
Bryan <bhtruong93@gmail.com>
2015-11-09 16:22:23 -0800

    test

--- build/server/server.js
+++ build/server/server.js
@@ -1,39 +1,12 @@
 var express = require('express');
-var mongodb = require('mongo');
-var mongoose = require('mongoose');
-// var bodyParser = require('body-parser');
-// var cookieParser = require('cookie-parser');
-// var bcrypt = require('bcryptjs');
-// var passport = require('passport');
 var app = express();
 var path = require('path');
-// var userController = require('./../src/userController');
 
-mongoose.connect('mongodb://localhost/life-analytics');
-mongoose.connection.once('open', function () {
-  console.log('Connected to life-analytics database');
-});
-
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './../client/')));
 // app.use(bodyParser());
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './../client/index.html'));
+ res.sendFile(path.join(__dirname, './../client/index.html'));
 });
 
-// app.get('/signup', userController.createUser, usercontroller.verifyUser, function (req, res) {
-//   res.redirect('/questionnaire');
-// });
-//
-// app.post('/login', usercontroller.verifyUser, function (req, res) {
-//   res.redirect('/dashboard');
-// });
-
-app.get('/dashboard', function (req, res) {
-  res.sendFile(path.join(__dirname, './../dashboard.html'));
-});
-
-
-
-app.listen(3000);
-module.exports = app;
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var reactify = require('reactify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var notify = require('gulp-notify');
+var nodemon = require('gulp-nodemon');
+
 
 function handleErrors() {
   var args = Array.prototype.slice.call(arguments);
@@ -14,41 +16,38 @@ function handleErrors() {
   //console.log('Compile error: ', args);
   this.emit('end'); //keeps gulp from hanging on this task
 }
-
 function buildScript(file, watch) {
   var props = {
-    entries : ['./build/' + file],
+    entries : ['./build/client/' + file],
     debug : true,
     transform : [reactify]
   };
-
   //watchify if watch set to true. otherwise browserify once
   var bundler = watch ? watchify(browserify(props)) : browserify(props);
-
   function rebundle(){
     var stream = bundler.bundle();
     return stream
       .on('error', handleErrors)
       .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./build/'));
+      .pipe(gulp.dest('./build/client/'));
   }
-
   bundler.on('update', function() {
     var updateStart = Date.now();
     rebundle();
     console.log('Updated!', (Date.now() - updateStart) + 'ms');
   })
-
   // run it once the first time buildScript is called
   return rebundle();
 }
-
 // run once
 gulp.task('scripts', function() {
-  return buildScript('/src/app.jsx', false);
+  return buildScript('./../src/app.jsx', true);
 });
-
 // run 'scripts' task first, then watch for future changes
 gulp.task('default', ['scripts'], function() {
-  return buildScript('/src/app.jsx', true);
+  return buildScript('./../src/app.jsx', true);
+});
+
+gulp.task('default', ['scripts'], function() {
+  return buildScript('./../src/app.jsx', true);
 });
