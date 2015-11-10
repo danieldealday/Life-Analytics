abb6a25dc0ce5eeaff4f8dcb3911f8ca6afab0a6
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2015-11-09 20:18:30 -0800

    added user goals

--- server/User/User.js
+++ server/User/User.js
@@ -13,9 +13,26 @@ var userInfo = new Schema({
 		unique: true, 
 		required: true 
 	},
-	lastName: { type: String, unique: true, required: true },
-	password: { type: String, unique: true, required: true },
-	email: { type: String, unique: true, required: true }
+	lastName: { 
+		type: String, 
+		unique: true, 
+		required: true
+	},
+	password: { 
+		type: String, 
+		unique: true, 
+		required: true 
+	},
+	email: { 
+		type: String, 
+		unique: true, 
+		required: true 
+	},
+	goal: { 
+		type: String, 
+		unique: true, 
+		required: true 
+	}
 });
 
 module.exports = mongoose.model('User', userInfo);
